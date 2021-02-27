2129a0377fa3d88e7adc4f04b6727b08c2b8158d
kusa-mochi <whoatemyapplepie@gmail.com>
2021-02-27 18:44:03 +0900

    make test earthquake data.

--- earthquake-transition/src/App.vue
+++ earthquake-transition/src/App.vue
@@ -1,17 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    App
+    <earthquake-chart :chart-data="chartData"></earthquake-chart>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import EarthquakeChart from "./components/EarthquakeChart.vue";
 
 export default {
   name: "App",
+  data() {
+    return {
+      chartData: [
+        {
+          datetime: "2011-03-11 14:00:00",
+          latitude: 35.681199,
+          longitude: 139.7653808,
+          depth: 10,
+          magnitude: 7
+        },
+        {
+          datetime: "2011-03-11 17:00:00",
+          latitude: 34.7008975,
+          longitude: 135.4950519,
+          depth: 20,
+          magnitude: 5.5
+        },
+        {
+          datetime: "2011-03-11 20:00:00",
+          latitude: 35.1692672,
+          longitude: 136.8809874,
+          depth: 30,
+          magnitude: 3.1
+        }
+      ]
+    };
+  },
   components: {
-    HelloWorld
+    EarthquakeChart
   }
 };
 </script>
---
+++ earthquake-transition/src/components/EarthquakeChart.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="earthquake-chart">
+    EarthquakeChart
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EarthquakeChart",
+  props: {
+    chartData: {
+      default: () => [],
+      required: true,
+      type: Array
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
